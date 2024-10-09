4f1a408dfd8e32663eb3c0ca00d99f33a0dd16b5
Dan Billson <daniel.billson@paddle.com>
2024-10-09 10:33:01 +0100

    feat(Simulations): narrow down simulation payload types

--- src/types/shared/index.ts
+++ src/types/shared/index.ts
@@ -35,3 +35,4 @@ export * from './custom-data';
 export * from './import-meta-response';
 export * from './simulation-event-request';
 export * from './simulation-event-response';
+export * from './simulation-payload';
