14103d223cf869793eea1b0cca0a4287cbcc007a
Dan Billson <daniel.billson@paddle.com>
2024-10-16 15:24:08 +0100

    chore: migrate eslintrc to js

---
+++ .eslintrc.cjs
@@ -0,0 +1,67 @@
+const { over } = require("lodash")
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    '**/__tests__/**',
+    "/dist",
+  ],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    '@typescript-eslint/semi': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/indent': 'off',
+    'generator-star-spacing': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/no-extraneous-class': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  },
+  overrides: [
+    {
+      files: ['**/src/**'],
+      excludedFiles: ['**/__tests__/**', '**/*.test.ts?(x)'],
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: false,
+            optionalDependencies: false,
+            peerDependencies: true,
+          },
+        ],
+      },
+    },
+  ]
+};
+
+
