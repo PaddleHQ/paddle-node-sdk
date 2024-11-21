a3a192d98c561b70194c54b7eaea24dbbc7122e9
Dan Billson <daniel.billson@paddle.com>
2024-11-21 13:00:36 +0000

    Customer Portal Sessions  (#74)
    * feat: add customer portal session resource
    
    * chore: remove unused mock
    
    Co-authored-by: davidgrayston-paddle <david.grayston@paddle.com>
    
    * test: add url assertion
    
    ---------
    
    Co-authored-by: davidgrayston-paddle <david.grayston@paddle.com>

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -10,6 +10,7 @@ export * from './price/index.js';
 export * from './transaction/index.js';
 export * from './adjustment/index.js';
 export * from './customer/index.js';
+export * from './customer-portal-session/index.js';
 export * from './business/index.js';
 export * from './subscription/index.js';
 export * from './address/index.js';
